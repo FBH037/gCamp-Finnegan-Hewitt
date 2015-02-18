78f8acf537e91bbce876c49a6868ed133c44eb26
Finnegan Hewitt <fbhewitt@gmail.com>
2015-02-18 13:29:39 -0700

    User must fill fields in user CRUD and use a unique email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ActiveRecord::Base
 def full_name
 first_name + " " + last_name
 end
+
+validates :first_name, presence: true
+validates :last_name, presence: true
+validates :email, presence: true, uniqueness: true
+
+
 end
