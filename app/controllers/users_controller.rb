692702b8d00d3a62cc71db287d91f77a1f6b9dae
Finnegan Hewitt <fbhewitt@gmail.com>
2015-02-18 08:47:20 -0700

    User must enter task description

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,20 +9,21 @@ class UsersController < ApplicationController
   end
 
   def edit
-
   end
+
   def show
     @user = User.find(params[:id])
   end
 
   def create
     @user = User.new(user_params)
-  if @user.save
-    redirect_to users_path, notice: 'User was successfully created.'
-  else
-    render :new
+    if @user.save
+      redirect_to users_path, notice: 'User was successfully created.'
+    else
+      render :new
+    end
   end
-  end
+
   def update
     if @user.update(user_params)
       redirect_to users_path, notice: 'User was successfully updated.'
@@ -46,5 +47,4 @@ private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email)
   end
-
 end
